13b4060be6ed18ad4a534427ab6d4d788cf48923
Filip Ušković <filipuskovic@Filips-MacBook-Air.local>
2023-08-14 15:24:22 +0200

    ignoreErrors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript:{
+    ignoreBuildErrors: true,
+  },
     experimental: {
         // ovo mora biti true da mozemo koristi mongodb mongoose
       serverActions: true,
